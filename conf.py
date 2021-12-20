52044e5d09f8c562209d832b9270e7fcacd19357
Per Joachims <p.joachims@gmail.com>
2021-12-20 11:58:38 +0100

    Fixing accxdiental remove

--- conf.py
+++ conf.py
@@ -52,7 +52,7 @@ exclude_patterns = ["old/"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = " "
+html_theme = "sphinx_book_theme"
 html_title = "Notes on ML & Stat"
 # html_logo = "link/to/logo"
 # html_theme_options = {
